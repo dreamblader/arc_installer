8cbd076f7ebb30871adcaf99997e4fc83e6d4e19
Dreamblader <carlosgabrielmaster@gmail.com>
2024-06-18 16:47:18 -0300

    added fluxogram dia chart and some extra stuff

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
 use druid::widget::Button;
-use druid::{AppLauncher, Widget, WindowDesc};
+use druid::{AppLauncher, Data, Widget, WindowDesc};
 
 mod loader;
 
-fn build_ui() -> impl Widget<()> {
+#[derive(Clone, Data)]
+struct UiData {
+    x:u8
+}
+
+fn build_ui() -> impl Widget<UiData> {
     Button::new("LOAD").on_click(|_,_,_| { loader::plugin_loader::load_plugins()})
 }
 
 fn main() {
     let main_window = WindowDesc::new(build_ui())
         .window_size((600.0, 400.0))
-        .title("Guild Wars 2 Plugin Manager");
-    let initial_data = ();
+        .title("Guild Wars 2 - Arc DPS Manager");
+    
+    let ui_data = UiData{x: 0};
 
     AppLauncher::with_window(main_window)
-        .launch(initial_data)
+        .launch(ui_data)
         .expect("Failed to launch application");
 }
+
+//FIND THIS FILE: "gw2-64.exe" to add dll a the same folder of it
